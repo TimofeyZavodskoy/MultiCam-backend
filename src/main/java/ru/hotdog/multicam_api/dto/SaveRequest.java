8d286d216e6bd401e81470a23915f8b0f4e8eb37
TimofeyZavodskoy <ambrasovskiytimofey@gmail.com>
2026-05-07 01:48:15 +0700

    feat: создан dto 'избранного'

--- src/main/java/ru/hotdog/multicam_api/dto/SaveRequest.java
+++ src/main/java/ru/hotdog/multicam_api/dto/SaveRequest.java
@@ -1,4 +1,10 @@
 package ru.hotdog.multicam_api.dto;
 
+import lombok.Data;
+
+@Data
 public class SaveRequest {
+    private String imageUrl;
+    private OCRResponse clientJson;
+    private String category;
 }
