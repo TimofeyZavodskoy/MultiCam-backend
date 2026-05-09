3b74c683cecc3c0a3459d715010f92e7f1305896
TimofeyZavodskoy <ambrasovskiytimofey@gmail.com>
2026-05-09 15:43:15 +0700

    feat: контроллер для сохранения карточек товаров

--- src/main/java/ru/hotdog/multicam_api/controller/SavedResult.java
+++ src/main/java/ru/hotdog/multicam_api/controller/SavedResult.java
@@ -4,33 +4,34 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import ru.hotdog.multicam_api.dto.SaveRequest;
 import ru.hotdog.multicam_api.entity.SaveResultEntity;
-import ru.hotdog.multicam_api.repository.SaveResultRepo;
-import tools.jackson.databind.ObjectMapper;
+import ru.hotdog.multicam_api.service.UserService;
+
+import java.security.Principal;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/save")
 @Slf4j
 public class SavedResult {
-    private final SaveResultRepo saveResultRepo;
-    private final ObjectMapper objectMapper;
+
+    private final UserService userService;
 
     @PostMapping("/like")
-    public ResponseEntity<String> saveResult(@RequestBody SaveRequest request) {
-        try {
-            SaveResultEntity result = new SaveResultEntity();
-            result.setImageUrl(request.getImageUrl());
-            result.setCategory(request.getCategory());
+    public Mono<ResponseEntity<String>> saveResult(@RequestBody SaveRequest request, Principal principal) {
+        return userService.saveResult(request, principal.getName())
+                .map(saved -> ResponseEntity.ok("Успешно сохранено!"))
+                .onErrorResume(e -> {
+                    log.error("Save error", e);
+                    return Mono.just(ResponseEntity.status(500).body("Ошибка: " + e.getMessage()));
+                });
+    }
 
-            String compactJsonString = objectMapper.writeValueAsString(request.getClientJson());
-            result.setJsonData(compactJsonString);
-
-            saveResultRepo.save(result);
-            return ResponseEntity.ok("Успешно сохранено!");
-        } catch (Exception e) {
-            return ResponseEntity.status(500).body("Ошибка: " + e.getMessage());
-        }
+    @GetMapping("/likes/all")
+     public Flux<SaveResultEntity> getAllLikes(Principal principal) {
+        return userService.getLikes(principal.getName());
     }
 }
