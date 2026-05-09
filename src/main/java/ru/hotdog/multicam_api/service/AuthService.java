376ab88d57d8f82c962ed167cbdd69d2b818cfce
TimofeyZavodskoy <ambrasovskiytimofey@gmail.com>
2026-05-09 15:57:36 +0700

    feat! :добаленеи метода создания гостевого аккаунта

--- src/main/java/ru/hotdog/multicam_api/service/AuthService.java
+++ src/main/java/ru/hotdog/multicam_api/service/AuthService.java
@@ -1,6 +1,8 @@
 package ru.hotdog.multicam_api.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
@@ -9,6 +11,10 @@ import ru.hotdog.multicam_api.dto.Signup;
 import ru.hotdog.multicam_api.entity.UserEntity;
 import ru.hotdog.multicam_api.repository.UserRepo;
 import ru.hotdog.multicam_api.security.JwtConfig;
+import ru.hotdog.multicam_api.service.impl.UserDetailsImpl;
+
+import java.util.ArrayList;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -24,7 +30,7 @@ public class AuthService {
                         return Mono.error(new RuntimeException("Почта уже занята"));
                     }
                     UserEntity user = new UserEntity();
-                    user.setName(signupRequest.getUsername());
+                    user.setName(signupRequest.getName());
                     user.setEmail(signupRequest.getEmail());
                     user.setPassword(passwordEncoder.encode(signupRequest.getPassword()));
                     return userRepo.save(user);
@@ -41,5 +47,32 @@ public class AuthService {
                     }
                     return Mono.just(jwtConfig.generateTokenForUser(user));
                 });
+    }
+
+public Mono<String> registerGuest(String uuid) {
+    String guestEmail = uuid + "@guest.local";
+
+    return userRepo.findByEmail(guestEmail)
+            .switchIfEmpty(Mono.defer(() -> {
+                UserEntity newGuest = new UserEntity();
+                newGuest.setName(uuid);
+                newGuest.setEmail(guestEmail);
+                newGuest.setGuest(true);
+                newGuest.setPassword(passwordEncoder.encode(uuid));
+                return userRepo.save(newGuest);
+            }))
+            .map(user -> {
+                UserDetailsImpl principal = UserDetailsImpl.build(user);
+                Authentication auth = new UsernamePasswordAuthenticationToken(
+                        principal,
+                        null,
+                        principal.getAuthorities()
+                );
+
+                return jwtConfig.generateToken(auth);
+            });
+}
+    public Mono<String> loginAfterUpgrade(UserEntity user) {
+        return Mono.just(jwtConfig.generateTokenForUser(user));
     }
 }
