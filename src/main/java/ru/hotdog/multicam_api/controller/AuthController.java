6d3942589fd0e86ce39c09a9f82469f9333d5be7
TimofeyZavodskoy <ambrasovskiytimofey@gmail.com>
2026-05-24 15:06:34 +0700

    feat: добавлен эндпоинт для рефреша токена

--- src/main/java/ru/hotdog/multicam_api/controller/AuthController.java
+++ src/main/java/ru/hotdog/multicam_api/controller/AuthController.java
@@ -3,19 +3,13 @@ package ru.hotdog.multicam_api.controller;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
-import ru.hotdog.multicam_api.dto.GuestRequest;
-import ru.hotdog.multicam_api.dto.Signin;
-import ru.hotdog.multicam_api.dto.Signup;
+import ru.hotdog.multicam_api.dto.*;
 import ru.hotdog.multicam_api.service.AuthService;
 import ru.hotdog.multicam_api.service.UserService;
 
 import java.security.Principal;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/auth")
@@ -33,29 +27,39 @@ public class AuthController {
     }
 
     @PostMapping("/signup/guest")
-    public Mono<ResponseEntity<String>> registerGuest(@RequestBody GuestRequest request) {
+    public Mono<ResponseEntity<TokenPair>> registerGuest(@RequestBody GuestRequest request) {
         return authService.registerGuest(request.getUuid())
-                .map(tokenResponse -> ResponseEntity.ok(tokenResponse))
-                .onErrorResume(e -> Mono.just(ResponseEntity.status(500).body("Ошибка: " + e.getMessage())));
-    }
-
-    @PostMapping("/upgrade")
-    public Mono<ResponseEntity<String>> upgradeAccount(
-            @RequestBody Signup request,
-            Principal principal
-    ) {
-        String guestEmail = principal.getName();
-
-        return userService.upgradeGuest(guestEmail, request.getEmail(), request.getPassword(), request.getName())
-                .flatMap(authService::loginAfterUpgrade)
                 .map(ResponseEntity::ok)
-                .onErrorResume(e -> Mono.just(ResponseEntity.status(400).body(Map.of("error", e.getMessage()).toString())));
+                .onErrorResume(e -> Mono.just(ResponseEntity.status(500).build()));
     }
 
     @PostMapping("/signin")
-    public Mono<ResponseEntity<String>> signin(@Valid @RequestBody Signin signinRequest) {
+    public Mono<ResponseEntity<TokenPair>> signin(@Valid @RequestBody Signin signinRequest) {
         return authService.authUser(signinRequest)
                 .map(ResponseEntity::ok)
-                .onErrorResume(ex -> Mono.just(ResponseEntity.badRequest().body(ex.getMessage())));
+                .onErrorResume(ex -> Mono.just(ResponseEntity.badRequest().build()));
+    }
+
+    @PostMapping("/refresh")
+    public Mono<ResponseEntity<TokenPair>> refresh(@RequestBody RefreshRequest request) {
+        return authService.refresh(request)
+                .map(ResponseEntity::ok)
+                .onErrorResume(ex -> Mono.just(ResponseEntity.status(401).build()));
+    }
+
+    @PostMapping("/upgrade")
+    public Mono<ResponseEntity<TokenPair>> upgradeAccount(
+            @RequestBody Signup request,
+            Principal principal
+    ) {
+        return userService.upgradeGuest(
+                        principal.getName(),
+                        request.getEmail(),
+                        request.getPassword(),
+                        request.getName()
+                )
+                .flatMap(authService::loginAfterUpgrade)
+                .map(ResponseEntity::ok)
+                .onErrorResume(e -> Mono.just(ResponseEntity.status(400).build()));
     }
 }
