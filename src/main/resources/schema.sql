5fb9d5938a2fa993fbd2afe13e656cb2ddf4b090
TimofeyZavodskoy <ambrasovskiytimofey@gmail.com>
2026-05-24 15:03:59 +0700

    feat: добавлена таблица refresh токенов

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -11,7 +11,15 @@ CREATE TABLE IF NOT EXISTS saved_result (
     id                BIGSERIAL PRIMARY KEY,
     image_url         VARCHAR(1024),
     json_data         TEXT,
-    user_id           BIGINT REFERENCES users(id),
+    user_id           BIGINT REFERENCES users(id) ON DELETE CASCADE,
     category          VARCHAR(100),
     created_at        TIMESTAMP DEFAULT NOW()
 );
+
+CREATE TABLE IF NOT EXISTS refresh_tokens (
+    id                BIGSERIAL PRIMARY KEY,
+    user_id           BIGINT REFERENCES users(id) ON DELETE CASCADE,
+    token             VARCHAR(512) UNIQUE NOT NULL,
+    expires_at        TIMESTAMP NOT NULL,
+    created_at        TIMESTAMP DEFAULT NOW()
+    );
